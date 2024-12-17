f0afa3351a25fd2e66c8fba9605be0ef59d7aef0
jpfleischer <70083705+jpfleischer@users.noreply.github.com>
2024-12-17 00:24:03 -0500

    Update script.sql

--- DB/script.sql
+++ DB/script.sql
@@ -1,7 +1,7 @@
 CREATE TABLE messages (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    content VARCHAR(255) NOT NULL,
-    sender VARCHAR(100) NOT NULL,
+    content VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    sender VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     coordinates POINT
-);
+) CHARACTER SET=utf8mb4 COLLATE=utf8mb4_general_ci;
